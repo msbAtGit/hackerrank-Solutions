d79b46e578233a472c79d93918384ec8c8ce58cb
msbAtGit <bharathsound@gmail.com>
2024-03-20 07:30:37 +0530

    Adding the tested solution for the kangaroo problem

--- kangaroo-jumps.js
+++ kangaroo-jumps.js
@@ -7,49 +7,53 @@
  *  2. INTEGER v1
  *  3. INTEGER x2
  *  4. INTEGER v2
+ * CODE Not working for 63 8 94 3
  */
 
 function kangaroo(x1, v1, x2, v2) {
   // Write your code here
+  while (x1 != x2 ) {
+      // No condition
+      if(x2 > x1 && v2 > v1) {
+          return "NO";
+      }
 
-  // No condition
-  if(x2 > x1 && v2 > v1) {
-      return "NO";
+      if(x1 > x2 && v1 > v2) {
+          return "NO";
+      }
+
+      if(x1 != x2 && v1 == v2) {
+         return "NO";
+      }
+      // First check if the Kangaroo can ever catch up
+
+      // The below solution is correct but its not efficient.
+      // while (x1 != x2) {
+      //     x1 = x1 + v1;
+      //     x2 = x2 + v2
+      // }
+      // if(x1 == x2) {
+      //     return "YES";
+      // }
+      // else {
+      //     return "NO";
+      // }
+
+      let r1 = x1 + v1;
+
+      let r2 = x2 + v2;
+
+      if(r1 % r2 == 0 || r2 %r1 == 0) {
+          return "YES";
+      }
+      else {
+          x1 = x1 + v1;
+          x2 = x2 + v2;
+      }
+
   }
 
-  if(x1 > x2 && v1 > v2) {
-      return "NO";
-  }
-  // First check if the Kangaroo can ever catch up
-
-  // The below solution is correct but its not efficient.
-  // while (x1 != x2) {
-  //     x1 = x1 + v1;
-  //     x2 = x2 + v2
-  // }
-  // if(x1 == x2) {
-  //     return "YES";
-  // }
-  // else {
-  //     return "NO";
-  // }
-
-  let r1 = x1 + v1;
-
-  let r2 = x2 + v2;
-
-  if(r1 % r2 == 0 || r2 %r1 == 0) {
-      return "YES";
-  }
-
-  else if (r2 % v1 == 0|| r1 % v2 == 0) {
-    return "YES";
-  }
-  else {
-      return "NO";
-  }
-
+  return "YES";
 
 }
-
-console.log(kangaroo(0,3,4,2));
+console.log(kangaroo(43,2,70,2));
